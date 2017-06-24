8569a1bd33584f70768b0d65adb0a5f66544436f
Dylan6502 <dylanjpaul@gmail.com>
2017-06-23 23:30:53 -0400

    Add files via upload

--- ByteComparisonMain.java
+++ ByteComparisonMain.java
@@ -1,37 +1,50 @@
 import java.util.Scanner;
 import java.io.*;
 import javax.swing.*;
-import java.nio.file;
+import java.nio.file.Path;
+import java.nio.file.Files;
 
 public class ByteComparisonMain extends JFrame
 {
-	File dir;
-	Scanner in;
+	// Files
+	File firstFile;
+	File secondFile;
 	
-	//Strings
+	
+	// Strings
 	String rawInput;
 	String filePath;
+	String output;
+	
+	// Numbers
+	double firstSize;
+	double secondSize;
 	
 	public ByteComparisonMain()
 	{
-		rawInput = JOptionPane.showInputDialog("Enter 1st file name and path.");
-		filePath = rawInput;
+		rawInput = JOptionPane.showInputDialog("Enter 1st filename and path.");
+		File firstFile = new File(rawInput);
 		
-		try
-		{
-			PathMatcher matcher =
-				FileSystems.getDefault().getPathMatcher("glob:*.{java,class}");
-
-			Path filename = filePath;
-			if (matcher.matches(filename)) {
-			System.out.println(filename);
-			}
+		rawInput = JOptionPane.showInputDialog("Enter another filename");
+		File secondFile = new File(rawInput);
+		
+		if(firstFile.exists() && secondFile.exists()) {
 			
-		}catch(FileNotFoundException e){
-			JOptionPane.showMessageDialog(null,"Damn!!");
+			firstSize = firstFile.length();
+			secondSize = secondFile.length();
+			
+			if(firstSize == secondSize) {
+				
+				JOptionPane.showMessageDialog(null, "Files are the same.");
+			}
+			else
+				JOptionPane.showMessageDialog(null, "Files are different.");
+				
+			JOptionPane.showMessageDialog(null, firstSize + "\n" + secondSize);
 		}
-		
-	
+		else {
+			JOptionPane.showMessageDialog(null,"404");
+		}
 	
 	}
 	public static void main(String args[])throws IOException
